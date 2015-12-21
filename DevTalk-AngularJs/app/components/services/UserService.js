54b786c07a092eb9a8359cc3e0ba13f66963f0ff
David Grimm <davidgrimm@web.de>
2015-12-21 11:02:11 +0100

    Directiven verbessert

--- DevTalk-AngularJs/app/components/services/UserService.js
+++ DevTalk-AngularJs/app/components/services/UserService.js
@@ -1,43 +1,57 @@
-﻿(function() {
+﻿(function () {
 
     'use strict';
 
     var app = angular.module('DevTalk.common', []);
 
-    app.factory('UserService', [function(){
+    app.factory('UserService', [function () {
         var user = {};
 
         return {
             // The user will be logged in, if the username != password
             // if the password ends with '?' the user is an admin
-            login: function(username, password){
-                if(username !== password){
+            login: function (username, password) {
+                if (username !== password) {
                     user.name = username;
                     user.role = 'user';
                 }
-                if(password.match(/.*\?/)){
+                if (password.match(/.*\?/)) {
                     user.role = 'admin';
                 }
                 //quickes way to deep-clone a object, see https://jsperf.com/cloning-an-object/2
                 return JSON.parse(JSON.stringify(user));
             },
-            getUserByid: function(id)
-            {
-                return {"id":id,"name":"David", "email":"nospam@asd.de", "password":"****", "image":"images/avatar.jpg"};
+            getUserByid: function (id) {
+                return {
+                    "id": id,
+                    "name": "David",
+                    "email": "nospam@asd.de",
+                    "password": "****",
+                    "image": "images/avatar.jpg"
+                };
             },
-            getCurrentUser: function()
-            {
-                return {"id":"1","name":user.name, "email":"nospam@asd.de", "password":"****", "image":"images/avatar.jpg", "role":user.role};
+            getCurrentUser: function () {
+                return {
+                    "id": "1",
+                    "name": user.name,
+                    "email": "nospam@asd.de",
+                    "password": "****",
+                    "image": "images/avatar.jpg",
+                    "role": user.role
+                };
             },
-            logout: function(){
+            update: function (user) {
+
+            },
+            logout: function () {
                 user = {};
                 //quickes way to deep-clone a object, see https://jsperf.com/cloning-an-object/2
                 return JSON.parse(JSON.stringify(user));
             },
-            isAdmin: function(){
+            isAdmin: function () {
                 return user.role === 'admin';
             },
-            isUser: function(){
+            isUser: function () {
                 return user.role === 'user';
             }
         };
