ccae3a42356b1537d24f4b59d7d3e342ac4e8c61
David Grimm <davidgrimm@web.de>
2015-12-21 10:20:48 +0100

    Korrekturen der Directiven Eventtable und Profil

--- DevTalk-AngularJs/app/components/services/UserService.js
+++ DevTalk-AngularJs/app/components/services/UserService.js
@@ -21,9 +21,13 @@
                 //quickes way to deep-clone a object, see https://jsperf.com/cloning-an-object/2
                 return JSON.parse(JSON.stringify(user));
             },
-            getUserByid: function()
+            getUserByid: function(id)
             {
-                return {"name":"David", "email":"nospam@asd.de", "password":"****", "image":"images/avatar.jpg"};
+                return {"id":id,"name":"David", "email":"nospam@asd.de", "password":"****", "image":"images/avatar.jpg"};
+            },
+            getCurrentUser: function()
+            {
+                return {"id":"1","name":user.name, "email":"nospam@asd.de", "password":"****", "image":"images/avatar.jpg", "role":user.role};
             },
             logout: function(){
                 user = {};
