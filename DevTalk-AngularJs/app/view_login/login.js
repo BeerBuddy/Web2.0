56ef82147aee90e66eb2754f7fc03599b4dfe099
unknown <Andreas@BUI-NB1097.bui.materna.com>
2016-01-07 11:19:21 +0100

    Login-Register-Component auf Angular Directive umgestellt

--- DevTalk-AngularJs/app/view_login/login.js
+++ DevTalk-AngularJs/app/view_login/login.js
@@ -2,27 +2,29 @@
 
 angular.module('DevTalk.login', ['ngRoute'])
 
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/login', {
-            templateUrl: 'view_login/login.html',
-            controller: 'LoginCtrl'
-        });
-    }])
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/login', {
+        templateUrl: 'view_login/login.html',
+        controller: 'LoginCtrl'
+    });
+}])
 
-    .controller('LoginCtrl', ['$scope', '$location', 'UserService', function ($scope, $location, UserService) {
-        $scope.submit = function () {
-            if ($scope.user) {
+.controller('LoginCtrl', ['$scope', '$location', 'UserService', function ($scope, $location, UserService) {
+
+    $scope.submit = function (user) {
+        if (user) {
                 // only do something if we have a user
-                if ($scope.user.agb) {
+                if (user.agb) {
                     // someone wants to register!
-                    UserService.register($scope.user.username, $scope.user.email, $scope.user.password);
+                    UserService.register(user.username, user.email, user.password);
                 }
                 // sir, we have a login!
-                if (UserService.login($scope.user.email, $scope.user.password)) {
-                    $location.path('/allTalks');
-                } else {
+                if (UserService.login(user.email, user.password)) {
+                  $location.path('/allTalks');
+              } else {
                     // we have not found a user, so we redirect to login
                     $location.path('/login');
+                    alert("Überprüfen Sie ihre Login-Daten");
                 }
             }
         }
