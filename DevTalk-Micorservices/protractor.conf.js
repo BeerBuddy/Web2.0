05b701becedb39b6fe7a6efd9ae9261d76c3615e
davidgri <davidgrimm@web.de>
2016-01-22 12:22:01 +0100

    protractor fix

--- DevTalk-Micorservices/protractor.conf.js
+++ DevTalk-Micorservices/protractor.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     'browserName': 'chrome'
   },
 
-  baseUrl: 'http://localhost:8000/',
+  baseUrl: 'https://localhost:8000/',
 
   framework: 'jasmine',
  onPrepare: function() {      
