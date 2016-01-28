c3aa85065eb93814de8e7b240ea8a93363853610
feress <felix.ress@gmail.com>
2016-01-28 10:52:49 +0100

    Filter fix und _id in alle Talks

--- DevTalk-Micorservices/microservices/webserver/app/components/services/RecommendationService.js
+++ DevTalk-Micorservices/microservices/webserver/app/components/services/RecommendationService.js
@@ -2,6 +2,6 @@ var app = angular.module('DevTalk.recommendation', ['ngResource']);
 
 app.factory('RecommendationService', ['$resource', function($resource) {
     
-    return $resource('/api/recommendationService/recommendations/:userId', {userId: '@id'}, {
+    return $resource('/api/recommendationService/recommendations/:userId', {userId: '@_id'}, {
 	});
 }]);
